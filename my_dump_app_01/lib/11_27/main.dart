3ff048c0507c05d49b98dae027e9236c40101004
sc2bat <izar_dero@naver.com>
2023-11-27 17:34:16 +0900

    add simple logger dependency

---
+++ my_dump_app_01/lib/11_27/main.dart
@@ -0,0 +1,11 @@
+import 'package:simple_logger/simple_logger.dart';
+
+final log = SimpleLogger();
+
+void main() {
+  log.info('hello world');
+  log.info('hello world');
+  log.info('hello world');
+  log.info('hello world');
+  log.info('hello world');
+}
