0eb342d11ab810fb8c7b4006af8884168221c245
sc2bat <izar_dero@naver.com>
2023-12-01 15:33:02 +0900

    1201 done

--- my_dump_app_01/lib/12_01/00_memory.dart
+++ my_dump_app_01/lib/12_01/00_memory.dart
@@ -1,24 +1,24 @@
-import 'package:my_dump_app_01/12_01/hero_1201.dart';
-import 'package:my_dump_app_01/12_01/sword_1201.dart';
-import 'package:my_dump_app_01/12_01/wizard_1201.dart';
-import 'package:simple_logger/simple_logger.dart';
+// import 'package:my_dump_app_01/12_01/hero_1201.dart';
+// import 'package:my_dump_app_01/12_01/sword_1201.dart';
+// import 'package:my_dump_app_01/12_01/wizard_1201.dart';
+// import 'package:simple_logger/simple_logger.dart';
 
-final log = SimpleLogger();
+// final log = SimpleLogger();
 
-void main() {
-  final p1 = Person();
+// void main() {
+//   final p1 = Person();
 
-  final fireSword = Sword1201(name: 'fireSword', damage: 10);
+//   final fireSword = Sword1201(name: 'fireSword', damage: 10);
 
-  final hero1 = Hero1201(name: '홍길동', hp: 100);
-  final hero2 = hero1;
+//   final hero1 = Hero1201(name: '홍길동', hp: 100);
+//   final hero2 = hero1;
 
-  final wizard1 = Wizard1201(name: 'wizard1', hp: 100);
+//   final wizard1 = Wizard1201(name: 'wizard1', hp: 100);
 
-  hero2.hp = 200;
+//   hero2.hp = 200;
 
-  log.info(hero1.hp);
-  log.info(hero2.hp);
-}
+//   log.info(hero1.hp);
+//   log.info(hero2.hp);
+// }
 
-class Person {}
+// class Person {}
--- my_dump_app_01/lib/12_01/game/cleric_1201.dart
+++ my_dump_app_01/lib/12_01/game/cleric_1201.dart
@@ -1,48 +1,86 @@
-// 성직자는 용사 처럼 이름과 HP를 가지고 있고, 추가로 마법을 사용하기 위한 MP를 가지고 있다.
-// 연습 1-1에서 작성한 내용이 없는 Cleric 클래스에 “이름", “HP”, “최대 HP”, “MP”, “최대 MP”를 속성으로 추가 하시오.
-// 또한 HP와 최대 HP는 정수로 초기치 50, MP와 최대 MP는 정수로 초기치 10으로 하고,
-// 최대 HP와 최대 MP는 상수 필드로 선언 하시오.
+// ignore_for_file: avoid_print
+
+// ignore: slash_for_doc_comments
+/**
+ * 
+1장의 연습문제에서 작성한 Cleric클래스에 관하여, 2가지 수정을 행하시오. ㅇ
+  현시점의 Cleric 클래스의 정의에는, 각 인스턴스별로 최대 HP와 최대 MP 필드에 정보를 가지고 있습니다. 
+  하지만, 모든 성직자의 최대 HP 는 50, 최대 MP 는 10으로 정해져 있어, 각 인스턴스가 각각의 정보를 가지는 것은 메모리 낭비이다. 
+  그래서, 최대 HP, 최대 MP의 필드가 각 인스턴스별로 있지 않도록, 필드 선언에 적절한 키워드를 추가 하던지 말던지 자유
+
+2. 아래의 방침에 따라, 생성자를 추가 하시오
+  A) 이 클래스는 Cleric(“아서스", hp: 40, mp: 5) 와 같이, 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다 ㅇ
+  B) 이 클래스는 Cleric(“아서스", hp: 35) 와 같이, 이름과 HP만으로 지정하여 인스턴스화 할 수 있다. 이 때, MP는 최대 MP와 같은 값이 초기화 된다 ㅇ
+  C) 이 클래스는 Cleric(“아서스") 와 같이 이름만을 지정하여 인스턴스화 할 수 있다. 이 때, HP 와 MP 는 최대 HP와 최대 MP로 초기화 된다 ㅇ
+  D) 이 클래스는 Cleric() 과 같이 이름을 지정하지 않는 경우에는 인스턴스화 할 수 없다고 한다. (이름이 없는 성직자는 존재 할 수 없음) ㅇ
+  E) 생성자는 가능한 한 중복되는 코드가 없도록 작성한다 ??
+ */
+
 import 'dart:math';
 
 class Cleric1201 {
-  final int maxHp = 50;
-  final int maxMp = 10;
+  /// 1. maxHp, maxMp 가 각각의 정보를 가지지않고 정적 변수로 선언
+  static int maxHp = 50;
+  static int maxMp = 10;
 
+  /// 2-A 이름, hp, mp 을 지정하여 인스턴스화 가능
   String name;
-  int hp = 50;
-  int mp = 10;
+  int? hp;
+  int? mp;
 
-  Cleric1201(this.name);
+  Cleric1201.all({
+    required this.name,
+    required this.hp,
+    required this.mp,
+  });
 
-  // 성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.
+  Cleric1201.noMp({
+    required this.name,
+    required this.hp,
+  }) : mp = maxMp;
+
+  Cleric1201.yesName({
+    required this.name,
+  })  : hp = maxHp,
+        mp = maxMp;
+
+  /// 2-C 이름만을 지정하여 인스턴스화 가능
+
   void selfAid() {
-    // 끝나는 조건
-    if (mp < 5) {
+    if (mp! < 5) {
       return;
     }
 
-    mp = mp - 5; // mp -= 5
+    mp = mp! - 5;
     hp = maxHp;
   }
 
-  // 성직자는 “기도하기" (pray) 라는 행동을 취할 수 있고, 자신의 MP를 회복한다.
-  // 이 메소드는 인수에 “기도할 시간(초)"를 지정할 수 있고, 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
-
   int pray(int sec) {
-    // 나중에 계산하려고 미리 값을 저장해 둔다
-    final beforeMp = mp;
+    final beforeMp = mp!;
 
-    // 회복량은 기도한 시간(초)에 랜덤하게 0 ~ 2포인트의 보정을 한 양이다 (3초 기도하면 회복량은 3 ~ 5 포인트).
     final recoveryMp = sec + Random().nextInt(3);
 
-    mp += recoveryMp;
+    mp = mp ?? 0 + recoveryMp;
 
-    // 단, 최대 MP 보다 더 회복하는 것은 불가능 하다.
-    if (mp > maxMp) {
+    if (mp! > maxMp) {
       mp = maxMp;
     }
 
-    // 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
-    return beforeMp - mp;
+    return beforeMp - mp!;
   }
+
+  void showInfo() {
+    print('cleric name : $name hp : $hp mp : $mp ');
+  }
+}
+
+void main() {
+  final cle1 = Cleric1201.all(name: 'Aseosu', hp: 30, mp: 5);
+  cle1.showInfo();
+
+  final cle2 = Cleric1201.noMp(name: 'Aseosu', hp: 20);
+  cle2.showInfo();
+
+  final cle3 = Cleric1201.yesName(name: 'Aseosu');
+  cle3.showInfo();
 }
