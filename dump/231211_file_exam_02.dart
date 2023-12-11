9db40a3d8e5a77bdf060943ea4e99bc58a6ed247
sc2bat <izar_dero@naver.com>
2023-12-11 17:33:46 +0900

    tojson tojson

--- dump/231211_file_exam_02.dart
+++ dump/231211_file_exam_02.dart
@@ -1,10 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:simple_logger/simple_logger.dart';
-
-final log = SimpleLogger();
-
 /**
  * 다음과 같은 사원 클래스와 부서 클래스가 있습니다.
 
@@ -61,39 +57,42 @@ void main() {
 
   // company.txt 파일에 쓰는
   const filePath = 'dump/';
-  const fileName = 'company.txt';
-
-  final writeFile = File(filePath + fileName);
-  writeFile.writeAsStringSync(jsonToString);
-
-  final readString = writeFile.readAsStringSync();
-  log.info(readString); // {"name":"홍길동","age":41}
+  const fileName = 'company2.txt';
 
   final depart = Depart('총무부', employee);
 
   // 직렬화
   Map<String, dynamic> departJson = depart.toJson();
   String departJsonToString = jsonEncode(departJson);
-  log.info(departJsonToString);
+  print(departJsonToString);
 
   final writeDepartFile = File(filePath + fileName);
   writeDepartFile.writeAsStringSync(departJsonToString);
 
   final readDepartString = writeDepartFile.readAsStringSync();
-  log.info(readDepartString); //
+  print(readDepartString); //
 
   Map<String, dynamic> readDepartJson = jsonDecode(readDepartString);
 
   // 역직렬화
   try {
     final departReverseJson = Depart.fromJson(readDepartJson);
-    log.info('qwerqwer');
-    log.info(departReverseJson); // Instance of 'Depart'
-    log.info(departReverseJson
+    print('qwerqwer');
+    print(departReverseJson); // Instance of 'Depart'
+    print(departReverseJson
         .toJson()); // {name: 총무부, leader: Instance of 'Employee'}
     // {name: 총무부, leader: {name: 홍길동, age: 41}}
-    // log.info(departReverseJson.toJson()['leader'].toJson()); // {name: 홍길동, age: 41}
+    // print(departReverseJson.toJson()['leader'].toJson()); // {name: 홍길동, age: 41}
   } catch (e) {
     throw Exception(e);
   }
+
+  /**
+Unhandled exception:
+Exception: type '_Map<String, dynamic>' is not a subtype of type 'Employee'
+#0      main (file:///D:/study_java/Dart/dump/231211_file_exam_02.dart:82:5)
+#1      _delayEntrypointInvocation.<anonymous closure> (dart:isolate-patch/isolate_patch.dart:297:19)
+#2      _RawReceivePort._handleMessage (dart:isolate-patch/isolate_patch.dart:184:12)
+   * 
+   */
 }
--- my_dump_app_01/lib/2312/12_11/02_file.dart
+++ my_dump_app_01/lib/2312/12_11/02_file.dart
@@ -19,4 +19,11 @@ void main() {
 
   // flutter 에서는 읽고쓰기가 자유롭게 안되는 듯
   // dart 에서 별도의 프로그램으로 실행됨을 확인함
+
+  File('file.txt').readAsString().then((String contents) {
+    print(contents);
+  });
+
+  final filename = 'file.txt';
+  var file = File(filename).writeAsString('some content');
 }
